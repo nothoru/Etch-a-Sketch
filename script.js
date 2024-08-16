eb76dde92b7f89b0745cd7942c19d35ab0128a54
nothoru <yuiishi022@gmail.com>
2024-08-16 15:50:58 +0800

    Added hover option
    this leaves a trail of color to the hovered column

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Square divs 16 x 16
 for (let rows = 0; rows < 16; rows++) {
   const row = document.createElement("div");
   row.classList.add("rows");
@@ -8,4 +9,14 @@ for (let rows = 0; rows < 16; rows++) {
     col.classList.add("column");
   }
   document.querySelector(".container").appendChild(row);
+}
+
+//Hover Effect
+//not onmouseover just mouseover
+const col = document.querySelectorAll(".column");
+
+for (const column of col) {
+  column.addEventListener("mouseover", () => {
+    column.setAttribute("style", "background: red");
+  });
 }
