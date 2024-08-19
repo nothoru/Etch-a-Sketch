1c5e7d7d61a5e22f8afc26fe2c6102d47071e6a3
nothoru <yuiishi022@gmail.com>
2024-08-19 18:39:02 +0800

    Added resize button
    So that user can define what size of the grids
    should be. a few arrangements on code which
    is putting the creation of grid and hover listener
    into one function

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-//Square divs 16 x 16
-for (let rows = 0; rows < 16; rows++) {
-  const row = document.createElement("div");
-  row.classList.add("rows");
+const container = document.querySelector(".page-container");
+const resize = document.querySelector("#resize");
 
-  for (let cols = 0; cols < 16; cols++) {
-    const col = document.createElement("div");
-    row.appendChild(col);
-    col.classList.add("column");
+//Square divs 16 x 16
+function createGrid(size) {
+  for (let rows = 0; rows < size; rows++) {
+    const row = document.createElement("div");
+    row.classList.add("rows");
+
+    for (let cols = 0; cols < size; cols++) {
+      const col = document.createElement("div");
+      row.appendChild(col);
+      col.classList.add("column");
+    }
+    document.querySelector(".container").appendChild(row);
   }
-  document.querySelector(".container").appendChild(row);
+
+  const col = document.querySelectorAll(".column");
+  for (const column of col) {
+    column.addEventListener("mouseover", () => {
+      column.setAttribute("style", "background: lightgreen");
+    });
+  }
 }
 
-//Hover Effect
-//not onmouseover just mouseover
-const col = document.querySelectorAll(".column");
+createGrid(16);
 
-for (const column of col) {
-  column.addEventListener("mouseover", () => {
-    column.setAttribute("style", "background: red");
-  });
+//Resizing the grid
+resize.addEventListener("click", () => {
+  removeGrid();
+
+  let size = parseInt(prompt("Size?"));
+
+  createGrid(size);
+});
+
+function removeGrid() {
+  const row = document.querySelectorAll(".rows");
+
+  for (const r of row) {
+    r.remove();
+  }
 }
